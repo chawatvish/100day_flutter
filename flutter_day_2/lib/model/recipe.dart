4868d0ce7e164b0540d72e53cb8aadffe7a5c250
Chawatvish Worrapoj <chawatvish.wo@gmail.com>
2022-06-12 09:36:55 +0700

    Restructure

---
+++ flutter_day_2/lib/model/recipe.dart
@@ -0,0 +1,9 @@
+class Recipe {
+  String label;
+  String imageUrl;
+
+  Recipe(this.label, this.imageUrl);
+  static List<Recipe> samples = [
+    Recipe('Spaghetti and Meatballs', 'assets/2')
+  ]
+}
